72760de86e6fa428a56de186deb0bc9da3df7037
jeevigowda2406-commits <jeevigowda2406@gmail.com>
2025-09-11 20:52:37 +0530

    chore: cleared main branch content

--- db/connection.py
+++
@@ -1,9 +0,0 @@
-# db/connection.py
-from flask_pymongo import PyMongo
-
-mongo = PyMongo()  # global mongo instance
-
-def init_db(app):
-    """Initialize PyMongo with the Flask app"""
-    mongo.init_app(app)
-    return mongo
